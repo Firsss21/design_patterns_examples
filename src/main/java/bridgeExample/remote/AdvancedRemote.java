4ede3214ad2ef3fb3ed773856d8b6aba56a48a9f
Grigoriy <grisha.firsov@mail.ru>
2020-12-21 18:12:00 +0700

    Added bridge pattern example

---
+++ src/main/java/bridgeExample/remote/AdvancedRemote.java
@@ -0,0 +1,16 @@
+package bridgeExample.remote;
+
+import bridgeExample.devices.Device;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+
+}
